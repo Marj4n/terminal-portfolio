0b55e233fb1bfe6b89fddff09434efddfe437a34
Marjan02 <kevinzevalja@gmail.com>
2023-05-21 17:44:52 +0700

    responsive mobile

--- src/components/ps1/Ps1.tsx
+++ src/components/ps1/Ps1.tsx
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from "react"
+import { isMobile } from "@/utils/isMobile"
 import { useTheme } from "@/utils/themeProvider"
 
 export const Ps1 = () => {
   const [hostname, setHostname] = useState("")
   const { theme } = useTheme()
+  const _isMobile = isMobile()
 
   useEffect(() => {
-    if (typeof window !== undefined) {
+    if (typeof window !== "undefined") {
       setHostname(window.location.hostname)
     }
   }, [])
+
+  if (typeof window === "undefined" || _isMobile) {
+    return (
+      <div>
+        <span
+          style={{
+            color: theme.green,
+          }}
+        >
+          guest
+        </span>
+        {">>>"}
+      </div>
+    )
+  }
 
   return (
     <div>
