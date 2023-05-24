8b6891c5fec70f6465125f8a61dd3496db6b1521
Marjan02 <kevinzevalja@gmail.com>
2023-05-24 14:06:38 +0700

    fix ascii

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack5: true,g
+  webpack5: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Exclude the font file from being processed by the default Next.js webpack loaders
       config.module.rules.push({
-        test: /\.(flf)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "static/fonts/[hash][ext][query]",
+        test: /\.(woff|woff2|eot|ttf|otf|svg|flf)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/fonts/[name].[hash].[ext]",
+          },
         },
       })
     }
-
     return config
   },
 }
